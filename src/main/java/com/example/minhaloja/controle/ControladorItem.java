7cfc9a5b262363af0a8239f4d95195455d515333
Aluno IFAL <aluno.ifal@LABS.RIOLARGO>
2019-08-23 11:29:56 -0200

    Verificações feitas na classe Item e em seu formulario

--- src/main/java/com/example/minhaloja/controle/ControladorItem.java
+++ src/main/java/com/example/minhaloja/controle/ControladorItem.java
@@ -1,12 +1,16 @@
 package com.example.minhaloja.controle;
 
+import javax.validation.Valid;
+
 import com.example.minhaloja.modelo.Item;
 import com.example.minhaloja.repositorios.RepositorioItem;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @RestController
 public class ControladorItem{
@@ -15,15 +19,22 @@ public class ControladorItem{
     RepositorioItem repositorioItem;
 
     @RequestMapping("/formulario_item")
-    public ModelAndView formularioItem(){
+    public ModelAndView formularioItem(Item item){
         ModelAndView retorno = new ModelAndView("cadastroItens.html");
         return retorno;
     }
 
     @RequestMapping("/novo_item")
-    public ModelAndView novoItem(Item item){
-        repositorioItem.save(item);
-        ModelAndView retorno = new ModelAndView("index.html");
+    public ModelAndView cadastroCliente(@Valid Item item, BindingResult bidingResult, RedirectAttributes redirect){
+        ModelAndView retorno;
+        if(bidingResult.hasErrors()){            
+            redirect.addFlashAttribute("item", item); 
+            retorno = new ModelAndView("cadastroItens.html");
+            return retorno;
+        }
+        retorno = new ModelAndView("redirect:/");
+        repositorioItem.save(item);  
+        redirect.addFlashAttribute("mensagem", "Item cadastrado com sucesso!");     
         return retorno;
     }
 }
--- src/main/java/com/example/minhaloja/modelo/Item.java
+++ src/main/java/com/example/minhaloja/modelo/Item.java
@@ -1,9 +1,15 @@
 package com.example.minhaloja.modelo;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Item {
@@ -11,7 +17,12 @@ public class Item {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @NotBlank(message = "O nome não pode ser vazio.")
+    @Size(min = 2, max = 50, message = "O nome precisa ter entre {min} e {max} catacteres.")
     private String nome;
+    
+    @NotNull(message = "O preço não pode ser vazio.")
+    @DecimalMin("0.1") 
     private double preco;
 
     public Item(){
