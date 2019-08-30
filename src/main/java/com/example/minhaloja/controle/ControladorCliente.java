2f666130c8f2e68282b616bc4555c44c2b8ba738
Anderson <adnrepper@gmail.com>
2019-08-30 01:28:44 -0300

    Alterações nas classes de controle pra mapeamento do cardapio e da navbar

--- src/main/java/com/example/minhaloja/controle/ControladorCliente.java
+++ src/main/java/com/example/minhaloja/controle/ControladorCliente.java
@@ -1,20 +1,11 @@
 package com.example.minhaloja.controle;
 
-import java.util.Set;
-
-import javax.validation.ConstraintViolation;
 import javax.validation.Valid;
-import javax.validation.Validation;
-import javax.validation.Validator;
 
 import com.example.minhaloja.modelo.Cliente;
 import com.example.minhaloja.repositorios.RepositorioCliente;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
-import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -34,26 +25,22 @@ public class ControladorCliente {
 
     @RequestMapping("/formulario_cliente")
     public ModelAndView formularioCliente(Cliente cliente) {
-        ModelAndView retorno = new ModelAndView("cadastroCliente.html");        
+        ModelAndView retorno = new ModelAndView("cadastroCliente.html");
         return retorno;
     }
 
-    // @InitBinder
-    // protected void initbinder(WebDataBinder binder){
-    // binder.setValidator();
-    // }
-
     @RequestMapping("/novo_cliente")
-    public ModelAndView cadastroCliente(@Valid Cliente cliente, BindingResult bidingResult, RedirectAttributes redirect){
+    public ModelAndView cadastroCliente(@Valid Cliente cliente, BindingResult bidingResult,
+            RedirectAttributes redirect) {
         ModelAndView retorno;
-        if(bidingResult.hasErrors()){            
-            redirect.addFlashAttribute("cliente", cliente); 
+        if (bidingResult.hasErrors()) {
+            redirect.addFlashAttribute("cliente", cliente);
             retorno = new ModelAndView("cadastroCliente.html");
             return retorno;
         }
         retorno = new ModelAndView("redirect:/");
-        repositorioCliente.save(cliente);  
-        redirect.addFlashAttribute("mensagem", "Cliente cadastrado com sucesso!");     
+        repositorioCliente.save(cliente);
+        redirect.addFlashAttribute("mensagem", "Cliente cadastrado com sucesso!");
         return retorno;
     }
 
--- src/main/java/com/example/minhaloja/controle/ControladorItem.java
+++ src/main/java/com/example/minhaloja/controle/ControladorItem.java
@@ -1,40 +1,64 @@
 package com.example.minhaloja.controle;
-
 import javax.validation.Valid;
-
 import com.example.minhaloja.modelo.Item;
 import com.example.minhaloja.repositorios.RepositorioItem;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @RestController
-public class ControladorItem{
-    
+public class ControladorItem {
+
     @Autowired
     RepositorioItem repositorioItem;
 
     @RequestMapping("/formulario_item")
-    public ModelAndView formularioItem(Item item){
+    public ModelAndView formularioItem(Item item) {
         ModelAndView retorno = new ModelAndView("cadastroItens.html");
         return retorno;
     }
 
+    @RequestMapping("/cardapio")
+    public ModelAndView listar_itens(RedirectAttributes redirect) {
+        ModelAndView model = new ModelAndView("listar_itens.html");
+        Iterable<Item> itens = repositorioItem.findAll();
+        model.addObject("itens", itens);
+        return model;
+    }
+
+    @RequestMapping("/busca")
+    public ModelAndView buscar(String q){
+        ModelAndView model = new ModelAndView("listar_itens.html");
+        Iterable<Item> itens = repositorioItem.findByNomeContaining(q);
+        model.addObject("itens", itens);
+
+        return model;
+    }
+
     @RequestMapping("/novo_item")
-    public ModelAndView cadastroCliente(@Valid Item item, BindingResult bidingResult, RedirectAttributes redirect){
+    public ModelAndView cadastroItem(@Valid Item item, BindingResult bidingResult,
+            RedirectAttributes redirect) {
         ModelAndView retorno;
-        if(bidingResult.hasErrors()){            
-            redirect.addFlashAttribute("item", item); 
+        if (bidingResult.hasErrors()) {
+            redirect.addFlashAttribute("item", item);
             retorno = new ModelAndView("cadastroItens.html");
             return retorno;
         }
         retorno = new ModelAndView("redirect:/");
-        repositorioItem.save(item);  
-        redirect.addFlashAttribute("mensagem", "Item cadastrado com sucesso!");     
+        repositorioItem.save(item);
+        redirect.addFlashAttribute("mensagem", "Item cadastrado com sucesso!");
         return retorno;
+    }
+
+    @RequestMapping("/addItem")
+    public ModelAndView adicionar_item(RedirectAttributes redirect){
+        ModelAndView model = new ModelAndView("redirect:/cardapio");
+        redirect.addFlashAttribute("mensagem", "Item adicionado ao Carrinho!");
+        return model;
     }
 }
