655108c38275292170aa18cd68f208b144d02039
Anderson <adnrepper@gmail.com>
2019-08-30 01:33:17 -0300

    data.sql criado e já inserido itens e um cliente. Demais páginas apenas com style e actions modificados

--- src/main/java/com/example/minhaloja/repositorios/RepositorioItem.java
+++ src/main/java/com/example/minhaloja/repositorios/RepositorioItem.java
@@ -5,5 +5,7 @@ import com.example.minhaloja.modelo.Item;
 import org.springframework.data.repository.CrudRepository;
 
 public interface RepositorioItem extends CrudRepository<Item, Long>{
+
+    Iterable<Item> findByNomeContaining(String q);
     
 }
