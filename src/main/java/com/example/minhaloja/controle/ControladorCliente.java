37cf4a409d445a74c84bdd8965929652fcf9da85
Anderson <adnrepper@gmail.com>
2019-09-04 21:48:37 -0300

    Feito mapeamento de listar, excluir, atualizar e buscar.

--- src/main/java/com/example/minhaloja/controle/ControladorCliente.java
+++ src/main/java/com/example/minhaloja/controle/ControladorCliente.java
@@ -1,11 +1,12 @@
 package com.example.minhaloja.controle;
 
+import java.util.Optional;
 import javax.validation.Valid;
-
 import com.example.minhaloja.modelo.Cliente;
 import com.example.minhaloja.repositorios.RepositorioCliente;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -42,6 +43,51 @@ public class ControladorCliente {
         repositorioCliente.save(cliente);
         redirect.addFlashAttribute("mensagem", "Cliente cadastrado com sucesso!");
         return retorno;
+    }
+
+    @RequestMapping("/excluir/{id}")
+    public ModelAndView excluir(@PathVariable("id") Long id, RedirectAttributes redirect) {
+        Optional<Cliente> opcao = repositorioCliente.findById(id);
+        ModelAndView model = new ModelAndView("redirect:/listar_clientes");
+        if (opcao.isPresent()) {
+            Cliente cliente = opcao.get();
+            repositorioCliente.deleteById(cliente.getId());
+            redirect.addFlashAttribute("mensagem", "Cliente excluido com sucesso!");
+
+            return model;
+        }
+        return model;
+
+    }
+
+    @RequestMapping("/atualizar/{id}")
+    public ModelAndView atualizar(@PathVariable("id") long id){
+        ModelAndView model = new ModelAndView("cadastroCliente.html");
+        Optional<Cliente> opcao = repositorioCliente.findById(id);
+        if(opcao.isPresent()){
+            Cliente cliente = opcao.get();
+            model.addObject("cliente", cliente);
+            return model;
+        }
+
+        return model;
+    }
+
+    @RequestMapping("/listar_clientes")
+    public ModelAndView att(){
+        ModelAndView model = new ModelAndView("listar_clientes.html");
+        Iterable<Cliente> clientes = repositorioCliente.findAll();
+        model.addObject("clientes", clientes);
+        return model;
+    }
+
+    @RequestMapping("/buscar_cliente")
+    public ModelAndView buscar(String q){
+        ModelAndView model = new ModelAndView("listar_clientes.html");
+        Iterable<Cliente> clientes = repositorioCliente.findByNomeContaining(q);
+        model.addObject("clientes", clientes);
+
+        return model;
     }
 
 }
