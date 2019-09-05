4af55314bb9ea5f8e33e18746ed38c9905d403c2
Anderson <adnrepper@gmail.com>
2019-09-04 21:40:22 -0300

    Mais inserts

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,14 +1,16 @@
 INSERT INTO ITEM (ID, NOME, PRECO) 
 VALUES(1, 'Hamburguer', 19.99),
-(2, 'Lasanha', 5.00),
+(2, 'Lasanha', 5),
 (3, 'Pizza', 25.49),
-(4, 'Coxinha', 2.50),
-(5, 'Especial Pedreiro', 15.00),
+(4, 'Coxinha', 2.5),
+(5, 'Especial Pedreiro', 15),
 (6, 'Batata Frita', 1.99),
 (7, 'Sorvete', 4.99),
-(8, 'Coca-Cola', 5.00),
+(8, 'Coca-Cola', 5),
 (9, 'Fanta', 3.79),
-(10, 'Água', 1.50);
+(10, 'Água', 1.5);
 
-INSERT INTO CLIENTE (ID,ENDERECO,NOME) 
-VALUES(1, 'Anderson', 'Avenida TÓTÓ');
+INSERT INTO CLIENTE (ID, NOME, ENDERECO)
+VALUES(1, 'ANDERSON','RUA 1'),
+(2, 'Adilson','RUA DO CARINHA QUE MORA LOGO ALI'),
+(3, 'Jubileu','BOM DIA E CIA');
