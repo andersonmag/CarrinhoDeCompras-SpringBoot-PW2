f7f79eadd47ea0488dffc56eafb03482d735f187
Anderson <adnrepper@gmail.com>
2019-09-04 21:49:16 -0300

    Feito mapeamento de listar, excluir, atualizar e buscar.

--- src/main/java/com/example/minhaloja/controle/ControladorItem.java
+++ src/main/java/com/example/minhaloja/controle/ControladorItem.java
@@ -1,10 +1,11 @@
 package com.example.minhaloja.controle;
+
+import java.util.Optional;
 import javax.validation.Valid;
 import com.example.minhaloja.modelo.Item;
 import com.example.minhaloja.repositorios.RepositorioItem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,29 +21,12 @@ public class ControladorItem {
     @RequestMapping("/formulario_item")
     public ModelAndView formularioItem(Item item) {
         ModelAndView retorno = new ModelAndView("cadastroItens.html");
+
         return retorno;
     }
 
-    @RequestMapping("/cardapio")
-    public ModelAndView listar_itens(RedirectAttributes redirect) {
-        ModelAndView model = new ModelAndView("listar_itens.html");
-        Iterable<Item> itens = repositorioItem.findAll();
-        model.addObject("itens", itens);
-        return model;
-    }
-
-    @RequestMapping("/busca")
-    public ModelAndView buscar(String q){
-        ModelAndView model = new ModelAndView("listar_itens.html");
-        Iterable<Item> itens = repositorioItem.findByNomeContaining(q);
-        model.addObject("itens", itens);
-
-        return model;
-    }
-
     @RequestMapping("/novo_item")
-    public ModelAndView cadastroItem(@Valid Item item, BindingResult bidingResult,
-            RedirectAttributes redirect) {
+    public ModelAndView cadastroItem(@Valid Item item, BindingResult bidingResult, RedirectAttributes redirect) {
         ModelAndView retorno;
         if (bidingResult.hasErrors()) {
             redirect.addFlashAttribute("item", item);
@@ -55,10 +39,49 @@ public class ControladorItem {
         return retorno;
     }
 
-    @RequestMapping("/addItem")
-    public ModelAndView adicionar_item(RedirectAttributes redirect){
-        ModelAndView model = new ModelAndView("redirect:/cardapio");
-        redirect.addFlashAttribute("mensagem", "Item adicionado ao Carrinho!");
+    @RequestMapping("/listar_itens")
+    public ModelAndView att() {
+        ModelAndView model = new ModelAndView("listar_itens.html");
+        Iterable<Item> itens = repositorioItem.findAll();
+        model.addObject("itens", itens);
         return model;
     }
+
+    @RequestMapping("/excluir_cadastro/{id}")
+    public ModelAndView excluir(@PathVariable("id") Long id, RedirectAttributes redirect) {
+        Optional<Item> opcao = repositorioItem.findById(id);
+        ModelAndView model = new ModelAndView("redirect:/listar_itens");
+        if (opcao.isPresent()) {
+            Item item = opcao.get();
+            repositorioItem.deleteById(item.getId());
+            redirect.addFlashAttribute("mensagem", "Item excluido com sucesso!");
+
+            return model;
+        }
+        return model;
+
+    }
+
+    @RequestMapping("/atualizar_cadastro/{id}")
+    public ModelAndView atualizar(@PathVariable("id") long id) {
+        ModelAndView model = new ModelAndView("cadastroItens.html");
+        Optional<Item> opcao = repositorioItem.findById(id);
+        if (opcao.isPresent()) {
+            Item item = opcao.get();
+            model.addObject("item", item);
+            return model;
+        }
+
+        return model;
+    }
+
+    @RequestMapping("/buscar_item")
+    public ModelAndView buscar(String q) {
+        ModelAndView model = new ModelAndView("listar_itens.html");
+        Iterable<Item> itens = repositorioItem.findByNomeContaining(q);
+        model.addObject("itens", itens);
+
+        return model;
+    }
+
 }
