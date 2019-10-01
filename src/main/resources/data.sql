b912110e7f190622ef9230abccaae2dfe64bda4c
Anderson <adnrepper@gmail.com>
2019-09-30 22:23:16 -0300

    Itens adicionados também no data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,14 @@
 INSERT INTO ITEM (ID, NOME, PRECO, NOME_IMAGEM) 
-VALUES(1, 'Hamburguer', 20, 'hamburguer.png'),
+VALUES(1, 'Hamburguer', 10, 'hamburguer.png'),
 (2, 'Coxinha', 2.5, 'coxinha.png'),
 (3, 'Batata Frita', 2, 'batata-frita.png'),
-(4, 'Pepsi', 4, 'pepsi.png');
+(4, 'Pepsi', 4, 'pepsi.png'),
+(5, 'Combo(Coca,Hamburguer e Batata)', 22, 'combo.png'),
+(6, 'Renatão', 4.50, 'renatao.jpeg'),
+(7, 'Pizza - Calabresa e Queijo', 19.90, 'pizza.png'),
+(8, 'Coca(500ML)', 3.50, 'coca.png'),
+(9, 'Pastel', 2.50, 'pastel.png'),
+(10, 'Ramen do Naruto', 8.80, 'ramen.png');
 
 INSERT INTO CLIENTE (ID, NOME, ENDERECO)
 VALUES(1, 'ANDERSON', 'RUA 1'),
